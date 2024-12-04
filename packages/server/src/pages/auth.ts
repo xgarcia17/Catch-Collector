80ca8b19d26c22edb816d62e767b612a8f024a34
Xavier Garcia <xmangarcia17@gmail.com>
2024-12-04 14:32:24 -0800

    add mu-auth to index.html

--- packages/server/src/pages/auth.ts
+++ packages/server/src/pages/auth.ts
@@ -9,23 +9,50 @@ export class LoginPage {
           `
           import { define, Auth } from "@calpoly/mustang";
           import { LoginForm } from "/scripts/login-form.js";
+          import { Events } from "@calpoly/mustang";
   
           define({
             "mu-auth": Auth.Provider,
             "login-form": LoginForm
           })
+
+            window.relayEvent = Events.relay;
+
+            function toggleLightView(page, checked) {
+                page.classList.toggle("light-view", checked);
+            }
+
+            document.body.addEventListener("light-view", (event) => 
+                toggleLightView(event.currentTarget, event.detail.checked)
+            );
           `
         ],
-        styles: [
-          css`
-            /* your CSS here */
-          `
+        stylesheets: [
+            "/styles/reset.css",
+            "/styles/tokens.css",
+            "/styles/page.css",
+            "/styles/trips.css"
         ],
+        // styles: [
+        //   css`
+        //     /* your CSS here */
+        //   `
+        // ],
         body: html`
           <body>
-            <mu-auth provides="blazing:auth">
+            <mu-auth provides="catch-collector:auth">
               <article>
-                <blz-header></blz-header>
+              <div class="header-title">
+                    <a href="/index.html"><h1>Catch Collector</h1></a>
+                    <h1>&nbsp&nbsp|&nbsp Login</h1>
+                </div>
+                <div class="header-contents">
+                    <label onchange="relayEvent(event, 'light-view', {checked: event.target.checked})">
+                        <input type="checkbox" autocomplete="off" />
+                        <h2>&nbspLight View</h2>
+                    </label>
+                    <h2>Xavier G.</h2>
+                </div>
                 <main class="page">
                   <login-form api="/auth/login">
                     <h3 slot="title">Sign in and go places!</h3>
